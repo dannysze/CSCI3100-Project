178d30d7cbadb89e007cde0a3fed663728a5d16f
eunicek4632 <20121G040@gmail.com>
2021-03-16 17:48:08 +0800

    connect to DB

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 // Node server
+var mysql = require('./calevents/node_modules/mysql');
+
+// Credentials
+var host = "csci3100-proj.cobhjw2xjj8l.us-east-1.rds.amazonaws.com";
+var user = "root";
+var pw = "csci3100";
+var db = "csci3100";
+
+var con = mysql.createConnection({
+  host: host,
+  user: user,
+  password: pw,
+  database: db
+});
+
+con.connect(function(err) {
+  if (err){
+      console.log(err);
+  }
+  console.log("Connected!");
+});
