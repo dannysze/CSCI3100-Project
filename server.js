58da6b0ec6877c8e075681d0fe6d1bb3d992580d
eunicek4632 <20121G040@gmail.com>
2021-03-16 19:50:43 +0800

    Render index.js file

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 // Node server
+var express = require('express');
+var app = express();
 var mysql = require('./calevents/node_modules/mysql');
+const pwd = require('path');
 
-// Credentials
+
+// Database Credentials
 var host = "csci3100-proj.cobhjw2xjj8l.us-east-1.rds.amazonaws.com";
 var user = "root";
 var pw = "csci3100";
@@ -20,3 +24,10 @@ con.connect(function(err) {
   }
   console.log("Connected!");
 });
+
+
+app.get('/', function(req, res) {
+    res.sendFile(pwd.join(__dirname + "/calevents/src/index.js"));
+});
+
+var server = app.listen(8000);
