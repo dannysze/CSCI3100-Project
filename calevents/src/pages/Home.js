71db60b07fcefe32b603fa5181fc6a690bca78ce
Danny Sze <dann.nn49825@gmail.com>
2021-03-17 23:07:28 +0800

    Update file structures

---
+++ calevents/src/pages/Home.js
@@ -0,0 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Home() {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+      </div>
+      <div className="navbar">
+
+      </div>
+      <div className="row">
+        <div className="col-8">
+
+        </div>
+        <div className="col-4">
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+        </div>
+      </div>
+      <div className="bg-danger">
+        <h6>Footer</h6>
+      </div>
+    </div>
+  )
+}
