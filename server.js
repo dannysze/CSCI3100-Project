ec4ceddc0a0d908468f5d2c9be69f58386369172
hermanli314 <68867871+hermanli314@users.noreply.github.com>
2021-03-18 01:43:19 +0800

    Separate database connection as an individual module

--- server.js
+++ server.js
@@ -1,29 +1,7 @@
 // Node server
 var express = require('express');
 var app = express();
-var mysql = require('./calevents/node_modules/mysql');
 const pwd = require('path');
-
-
-// Database Credentials
-var host = "csci3100-proj.cobhjw2xjj8l.us-east-1.rds.amazonaws.com";
-var user = "root";
-var pw = "csci3100";
-var db = "csci3100";
-
-var con = mysql.createConnection({
-  host: host,
-  user: user,
-  password: pw,
-  database: db
-});
-
-con.connect(function(err) {
-  if (err){
-      console.log(err);
-  }
-  console.log("Connected!");
-});
 
 
 app.get('/', function(req, res) {
@@ -31,3 +9,5 @@ app.get('/', function(req, res) {
 });
 
 var server = app.listen(8000);
+
+module.exports = app
