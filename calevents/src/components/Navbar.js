2db945d1fa7e34af39ef269db68478cda3469781
wmhui0102 <1155127512@link.cuh.edu.hk>
2021-03-18 22:02:38 +0800

    add dropbar

--- calevents/src/components/Navbar.js
+++ calevents/src/components/Navbar.js
@@ -10,13 +10,32 @@ const navItems = [
     {
         title:'My Calendar',
         url: '#',
-        cName: 'navi'
+        cName: 'drop'
     },
     {
         title:'Search',
         url: '#',
         cName: 'navi'
     },
+    
+]
+const dropItems = [
+  {
+    title:'Creat Event',
+    url: '#',
+    cName: 'drop-c'
+},
+{
+  title:'Edit',
+  url: '#',
+  cName: 'drop-c'
+},
+{
+title:'Remove?',
+url: '#',
+cName: 'drop-c'
+},
+
 ]
 
 
@@ -25,7 +44,21 @@ function Navbar() {
     <div className="navbar-n">
       {navItems.map((item, index) => {
         return(
-          <a className={item.cName} href={item.url}>{item.title}</a>
+          item.cName === 'drop' ?
+            <a className={item.cName} href={item.url}>{item.title}
+              <div className="dropcontent">
+                {dropItems.map((item, index) => {
+                  return(
+                    <a className={item.cName} herf={item.url}>{item.title}</a>
+
+                  )
+                })}
+            
+              </div>
+            </a>
+            :<a className={item.cName} href={item.url}>{item.title}</a>
+            
+          
         )
       })}
     </div>
