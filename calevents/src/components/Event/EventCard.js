a627e9595b54d2cade775ce89d5edd7092e395fb
wmhui0102 <1155127512@link.cuhk.edu.hk>
2021-04-09 16:31:26 +0800

    try eventcard

--- calevents/src/components/Event/EventCard.js
+++ calevents/src/components/Event/EventCard.js
@@ -1,14 +1,35 @@
 import PropTypes from 'prop-types'
 import "../../styles/components/Event/EventCard.css"
 
-const EventCard = ({event, onClick}) => {
+const EventCard = ({ event, onClick }) => {
     return (
-        <div className='eventCard' onClick={() => {onClick()}}>
+        <div className='eventCard' onClick={() => { onClick() }}>
+
+
+
             <h3 className='datetime'>{event.start_time}-{event.end_time}/{event.start_date}</h3>
             <h3 className='title'>{event.name}</h3>
             <p className='desc'>
                 {event.desc}
             </p>
+
+
+        </div>
+    )
+}
+
+const New_card = ({ event, onClick }) => {
+    return (
+        <div className='card-container'>
+            <div className='image-container'>
+                <img src="url" alt="event photo"></img>
+            </div>
+            <div className="Info-container">
+                <h3 className='title'>Event Name: {event.name}</h3>
+                <h3 className='Organizer'>Organizer: {event.organizer}</h3>
+                <h3 className='datetime'>Event time/ date: {event.start_time}-{event.end_time}/{event.start_date}</h3>
+                <h3 className='description-head'>Description:</h3>
+            </div>
         </div>
     )
 }
