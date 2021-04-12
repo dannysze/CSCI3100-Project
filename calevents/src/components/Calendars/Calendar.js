9ded26d2d447f9a03662714ca640f90169cca0a3
Danny Sze <dann.nn49825@gmail.com>
2021-04-13 02:13:06 +0800

    Calendar date fixed

--- calevents/src/components/Calendars/Calendar.js
+++ calevents/src/components/Calendars/Calendar.js
@@ -46,22 +46,11 @@ const takeMonth = (date = new Date()) => {
 }
 
 const sqlToJsDate = (sqlDate, sqlTime) => {
-  //sqlDate in SQL DATETIME format ("yyyy-mm-dd hh:mm:ss.ms")
+  
   var sqlDateArr1 = sqlDate.split("-");
-  //format of sqlDateArr1[] = ['yyyy','mm','dd hh:mm:ms']
   var sYear = sqlDateArr1[0];
   var sMonth = (Number(sqlDateArr1[1]) - 1).toString();
-  var sqlDateArr2 = sqlDateArr1[2].split("T");
-  //format of sqlDateArr2[] = ['dd', 'hh:mm:ss.ms']
-  var sDay = sqlDateArr2[0];
-  var sqlDateArr3 = sqlDateArr2[1].split(":");
-  //format of sqlDateArr3[] = ['hh','mm','ss.ms']
-  var sHour = sqlDateArr3[0];
-  var sMinute = sqlDateArr3[1];
-  var sqlDateArr4 = sqlDateArr3[2].split(".");
-  //format of sqlDateArr4[] = ['ss','ms']
-  var sSecond = sqlDateArr4[0];
-  var sMillisecond = sqlDateArr4[1];
+  var sDay = sqlDateArr1[2];
 
   var sqlTimeArr = sqlTime.split(":");
    
