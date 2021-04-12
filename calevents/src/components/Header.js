16194c5e9eedc2c73c3c5a0e57fe587b9e6c2d0b
N30B4N4N4 <59244217+N30B4N4N4@users.noreply.github.com>
2021-04-13 02:00:32 +0800

    added logout function, some fixes to login.js

--- calevents/src/components/Header.js
+++ calevents/src/components/Header.js
@@ -10,8 +10,13 @@ import '../styles/components/Header.css';
 const Header = () => {
 
   const [events, setEvents] = useState([]);
-  const {token} = useToken();
+  const {token, setToken} = useToken();
   const {user, setUser} = useContext(UserContext);
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    window.location.replace("/login");
+  };
 
   const headerItems = [
     {
@@ -31,7 +36,8 @@ const Header = () => {
         title: 'Log out ',
         url: '#',
         icon: <Icon.BoxArrowRight />,
-        cName: 'header-items'
+        cName: 'header-items',
+        onclick: () => {logout()}
     },
   ]
   const [showRedeem, setShowRedeem] = useState(false);
