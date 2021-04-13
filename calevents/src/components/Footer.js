4d3c48c0c9f87dd0419951c50c2d7096dc310541
wmhui0102 <1155127512@link.cuhk.edu.hk>
2021-04-14 00:37:31 +0800

    some edit on footer

--- calevents/src/components/Footer.js
+++ calevents/src/components/Footer.js
@@ -4,6 +4,7 @@ import '../styles/components/Footer.css';
 const Footer = () => {
   return (
     <div className="footer">
+      <h1>C a l E v e n t</h1>
       <p>Copyright &copy; All right reserved by CSCI 3100 Group E1 2021 </p>
       <a href="https://github.com/dannysze/CSCI3100-Project" target="_blank">
         <Github />
