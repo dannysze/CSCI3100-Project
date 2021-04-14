61b532935d93049cb4660557a7544bc33b9aca0b
eunicek4632 <20121G040@gmail.com>
2021-04-14 18:43:51 +0800

    /joined_events/:uID bug fixed

--- server.js
+++ server.js
@@ -314,7 +314,7 @@ app.post('/create_event', checkAuth ,upload.single('img'), function(req, res) {
 // tested
 app.get('/joined_events/:uID', function(req, res){
     var u_ID = req.params['uID'];
-    var sql = `SELECT event_id, name, start_date, start_time, end_date, end_time, description, venue, ticket, TEMP.img_loc as img_loc, U.username as organizer, U.user_id as organizer_id FROM csci3100.User U, (SELECT * FROM csci3100.Event E NATURAL JOIN csci3100.Event_Join J WHERE J.user_id = ?) TEMP WHERE U.user_id = TEMP.organizer;`
+    var sql = `SELECT event_id, name, start_date, start_time, end_date, end_time, description, venue, category, ticket, TEMP.img_loc as img_loc, U.username as organizer, U.user_id as organizer_id FROM csci3100.User U, (SELECT * FROM csci3100.Event E NATURAL JOIN csci3100.Event_Join J WHERE J.user_id = ?) TEMP WHERE U.user_id = TEMP.organizer;`
     con.query(sql, [u_ID], function(err, result){
         if (err) throw err;
         
@@ -323,7 +323,7 @@ app.get('/joined_events/:uID', function(req, res){
             // console.log(result);
         }
         else{
-            res.status(200).send("No events Found");
+            res.status(400).send("No events Found");
             // console.log(result);
         }
 
